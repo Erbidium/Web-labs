2b3a172ff8968dc2fcf111fe7c0239ea3029612d
Erbidium <OlexandrTanasienko@gmail.com>
2023-09-28 21:23:53 +0300

    Create basic app with 2 components and routing

---
+++ Lab2/app/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {Task1Component} from "./components/task1/task1.component";
+import {Task2Component} from "./components/task2/task2.component";
+
+const routes: Routes = [
+  { path: 'task1', component: Task1Component },
+  { path: 'task2', component: Task2Component }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
