90e2792ec034b2dbe6eb0d15713fac7563ef3ef5
Erbidium <OlexandrTanasienko@gmail.com>
2023-09-29 09:30:36 +0300

    Create table filled with numbers and changing cell color

--- Lab2/app/src/app/components/task2/task2.component.ts
+++ Lab2/app/src/app/components/task2/task2.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./task2.component.scss']
 })
 export class Task2Component {
+  rows: number[][] = [];
 
+  randomColor: string = '';
+
+  variant: number = 11;
+
+  ngOnInit(): void {
+    this.fillTable();
+  }
+
+  fillTable(): void {
+    let cellValue = 1;
+    for (let i = 0; i < 6; i++) {
+      const row = [];
+      for (let j = 0; j < 6; j++) {
+        row.push(cellValue++);
+      }
+      this.rows.push(row);
+    }
+  }
+
+  handleCellHover(cell: number) {
+    if (cell === this.variant) {
+      this.randomColor = this.getRandomColor();
+    }
+  }
+
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }
